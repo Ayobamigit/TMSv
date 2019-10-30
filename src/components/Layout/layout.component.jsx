54cd57c7009155aa8beb65bddb836181de27ca0a
Gbenga Olufeyimi <GbengaO@3lineng.com>
2019-10-30 14:22:38 +0100

    Slight correction to Mobile UI

--- src/components/Layout/layout.component.jsx
+++ src/components/Layout/layout.component.jsx
@@ -6,51 +6,56 @@ import Sidebar from '../Sidebar/sidebar.component';
 
 export default function Layout ({ children }) {
   const [sidebar, setSidebar] = useState(true);
-    useLayoutEffect(() => {
-      // Setting Sidebar Dynamically based on window screen width
-      if(window.innerWidth < 900){
-        setSidebar(false)
-      }
-    }, [])
-    useEffect(() => {
-      if(sidebar){
-        if(window.innerWidth < 900){
-          document.getElementById("main").style.marginLeft = "70%";
-          document.getElementById("mySidebar").style.width = "70%";
-        } else {
-          document.getElementById("main").style.marginLeft = "15%";
-          document.getElementById("mySidebar").style.width = "15%";
-        }
-        document.getElementById("mySidebar").style.display = "block";
-      } else {
-        document.getElementById("main").style.marginLeft = "0%";
-        document.getElementById("mySidebar").style.display = "none";
-      }
-    }, [sidebar])
-    const toggleSidebar = () => {
-      setSidebar(!sidebar);
+  useLayoutEffect(() => {
+    // Setting Sidebar Dynamically based on window screen width
+    if(window.innerWidth < 900){
+      setSidebar(false)
     }
-    return (
-      <React.Fragment>
-        <Header /> 
-        <div id="wrapper">
-          <div style={{display:'none'}} id="mySidebar">
-            <Sidebar />           
-          </div>
-          <div id="main" style={{position: 'relative'}}>
-            <div className="children">
-              <div 
-                className={sidebar ? "menu-toggle-btn open" : "menu-toggle-btn"}
-                onClick={toggleSidebar}
-              >
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-              {children}
+  }, [])
+  useEffect(() => {
+    if(sidebar){
+      if(window.innerWidth < 900){
+        document.getElementById("main").style.marginLeft = "70%";
+        document.getElementById("mySidebar").style.width = "70%";
+        document.getElementById("manipulateChildren").style.display = "none";
+      } else {
+        document.getElementById("main").style.marginLeft = "15%";
+        document.getElementById("mySidebar").style.width = "15%";
+      }
+      document.getElementById("mySidebar").style.display = "block";
+    } else {
+      document.getElementById("main").style.marginLeft = "0%";
+      document.getElementById("mySidebar").style.display = "none";
+      document.getElementById("manipulateChildren").style.display = "block";
+    }
+  }, [sidebar])
+  
+  const toggleSidebar = () => {
+    setSidebar(!sidebar);
+  }
+  return (
+    <React.Fragment>
+      <Header /> 
+      <div id="wrapper">
+        <div style={{display:'none'}} id="mySidebar">
+          <Sidebar />           
+        </div>
+        <div id="main" style={{position: 'relative'}}>
+          <div className="children">
+            <div 
+              className={sidebar ? "menu-toggle-btn open" : "menu-toggle-btn"}
+              onClick={toggleSidebar}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
+            <div id="manipulateChildren">
+              {children}
+            </div>              
           </div>
         </div>
-      </React.Fragment>
-    )
+      </div>
+    </React.Fragment>
+  )
 }
